bcffc773efc5d4b0732c38a8e3697d4095af7713
bikramjit7777 <bikramjit7777@gmail.com>
2026-05-05 13:10:18 -0400

    added login logic for controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,7 +16,30 @@ const register = async(req, res) => {
 }
 
 const login = async(req, res) => {
-    
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+
+        if(!user) {
+            return res.status(404).json({message: `User with username ${username} not found!`})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+
+        const token = jwt.sign(
+            {id: user._id, role: user.role}, 
+            process.env.JWT_SECRET,
+            {expiresIn: "1h"}
+        )
+
+        res.status(200).json({token});
+
+    } catch (err) {
+        res.status(500).json({message: "Something went wrong!"})
+    }
 }
 
 module.exports = {
